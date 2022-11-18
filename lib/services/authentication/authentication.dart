afbd5573e7ec079d6d7682640f3e46b29d716815
francesca <francesca.forbicini@mail.polimi.it>
2022-11-18 13:02:00 +0100

    Fix front end authentication.dart

--- lib/services/authentication/authentication.dart
+++ lib/services/authentication/authentication.dart
@@ -1,12 +1,34 @@
 import 'package:flutter/material.dart';
 
-class AutenticationWidget extends StatefulWidget{
+class AutenticationWidget extends StatelessWidget{
   const AutenticationWidget({super.key});
-}
 
-@override
-Widget build(BuildContext context){
-  return Scaffold(
 
-  )
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: SizedBox(
+          width: 300,
+          child: Column(
+            children: [
+              Center(
+                child: FittedBox(
+                  fit: BoxFit.fitWidth,
+                  child: Text(
+                    'ciao',
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 110
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+
+    );
+  }
 }
