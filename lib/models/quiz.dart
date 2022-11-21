c6b89c33ac34d0b00a6adab562613e610ca367d2
francesca <francesca.forbicini@mail.polimi.it>
2022-11-21 16:31:21 +0100

    Added screen Quizzes

--- lib/models/quiz.dart
+++ lib/models/quiz.dart
@@ -3,19 +3,11 @@ import 'package:dima_project/models/question.dart';
 import 'package:dima_project/models/user.dart';
 
 class Quiz {
-  final String id;
-  String genre;
-  String author;
-  final List<Question> questions;
-  final User winner;
-  final List<User> participants;
+  final String id = "";
+  String genre = "";
+  String author = "";
+  List<Question> questions = [];
+  //User winner;
+  List<User> participants = [];
 
-  Quiz({
-    required this.id,
-    this.genre = "",
-    this.author = "",
-    required this.questions,
-    required this.winner,
-    required this.participants
-  });
 }
---
+++ lib/screens/profile/quiz_screen.dart
@@ -0,0 +1,56 @@
+import 'package:dima_project/screens/profile/userprofile_screen.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import '../../models/answer.dart';
+import '../../models/quiz.dart';
+
+class QuizScreen extends StatelessWidget{
+  Quiz quiz;
+  List <Answer> answers;
+  QuizScreen({required this.quiz, required this.answers});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Quiz',
+      home: Scaffold(
+          backgroundColor: Color(0xFF101010),
+          appBar: AppBar(
+            title: Text('My Quizzes',style: TextStyle(fontSize: 30, color: Colors.lightGreen), ),
+
+            backgroundColor: Color (0xFF101010),
+            actions:<Widget> [
+              IconButton( icon: Icon(Icons.arrow_back, color: Colors.lightGreen), onPressed:(){ Navigator.push(context,
+                  MaterialPageRoute(
+                      builder: (context) => UserProfile()));
+              }),
+              IconButton(icon: Icon(Icons.add,color: Colors.lightGreen), onPressed:(){ Navigator.push(context,
+                 MaterialPageRoute(
+                    builder: (context) => UserProfile()));
+              }),
+
+            ],
+
+          ),
+          body:
+          ListView.builder(
+            itemCount: quiz.questions.length,
+            itemBuilder: (context, index) {
+              return ListTile(
+                  textColor: Colors.lightGreen,
+                  title: Text(quiz.questions[index].text,style: TextStyle(fontSize: 30)),
+                  subtitle: ListView.builder(
+                      itemCount: answers.length,
+                      itemBuilder: (context, index){
+                        return ListTile(
+                            title: Text(answers[index].text, style: TextStyle(fontSize: 20)),
+                        );
+                      },
+                  ),
+              );
+            },
+          )
+      ),
+    );
+  }
+}
