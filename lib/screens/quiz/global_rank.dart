ee37b54bd8c5a7e3987a8989711562e2602f7642
francesca <francesca.forbicini@mail.polimi.it>
2022-12-31 16:23:00 +0100

    global rank bug

--- lib/screens/quiz/global_rank.dart
+++ lib/screens/quiz/global_rank.dart
@@ -13,22 +13,6 @@ class MyQuiz extends StatelessWidget{
 
   @override
   Widget build(BuildContext context) {
-    final _appBar = AppBar(
-      backgroundColor: Color(0xFF101010),
-      leading: IconButton(
-        icon: Icon(Icons.arrow_back, color: Colors.lightGreen, size: 30),
-        onPressed: () =>
-        {
-          Navigator.push(
-              context, MaterialPageRoute(builder: (context) => UserProfile()))
-        },
-      ),
-      title: const AutoSizeText(
-        'Global Rank',
-        textAlign: TextAlign.center,
-        style: TextStyle(fontSize: 30),
-      ),
-    );
 
     final _screenHeight = MediaQuery
         .of(context)
@@ -38,37 +22,53 @@ class MyQuiz extends StatelessWidget{
         .of(context)
         .size
         .width;
-    final _appBarHeight = _appBar.preferredSize.height;
+   // final _appBarHeight = _appBar.preferredSize.height;
     final _statusBarHeight = MediaQuery
         .of(context)
         .padding
         .top;
-    final _height = _screenHeight - _appBarHeight - _statusBarHeight;
-    final radius = min(_height * 0.5 * 0.25, _screenWidth * 0.25);
+   // final _height = _screenHeight - _appBarHeight - _statusBarHeight;
+    //final radius = min(_height * 0.5 * 0.25, _screenWidth * 0.25);
     var photoUser;
     var users;
     var points;
     int i = 0;
     return Scaffold(
       backgroundColor: Color(0xFF101010),
-      appBar: _appBar,
+      appBar: AppBar(
+        backgroundColor: Color(0xFF101010),
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back, color: Colors.lightGreen, size: 30),
+          onPressed: () =>
+          {
+            Navigator.push(
+                context, MaterialPageRoute(builder: (context) => UserProfile()))
+          },
+        ),
+        title: const AutoSizeText(
+          'Global Rank',
+          textAlign: TextAlign.center,
+          style: TextStyle(fontSize: 30),
+        ),
+      ),
 
       body: Flexible(
           child: StreamBuilder<QuerySnapshot>(
               stream: FirebaseFirestore.instance
-                  .collection('Quiz')
-                  .where('quiz', isEqualTo: '12')
-                  .orderBy('points', descending: true)
-                  .snapshots(),
+                  .collection("Quiz").snapshots(),
               builder: (context, snapshot) {
                 if (snapshot.hasData) {
                   i = 0;
                   return ListView.builder(
                       itemCount: snapshot.data?.docs.length,
                       itemBuilder: (context, index) {
+                        QuerySnapshot? snap = snapshot.data; // Snapshot
+                        List<DocumentSnapshot> items = snap!.docs; // List of Documents
+                        DocumentSnapshot item = items[index];
                         if (index >= 1) {
-                            points = snapshot.data?.docs[index].get("points").toString();
-                            users = snapshot.data?.docs[index].get("user").toString();
+                            points = item["points"].toString();
+                            print(points);
+                            users = item["user"].toString();
                           //TODO how to retrieve things?
                         }
                         i++;
@@ -90,56 +90,56 @@ class MyQuiz extends StatelessWidget{
                                   .of(context)
                                   .size
                                   .width,
-                              child: Column(
-                                children: <Widget>[
-                                  Row(
-                                    children: <Widget>[
-                                      Padding(
-                                        padding: const EdgeInsets.only(
-                                            top: 10.0, left: 15.0),
-                                        child: Row(
-                                          children: <Widget>[
-                                            CircleAvatar(
-                                                child: Container(
-                                                    decoration: BoxDecoration(
-                                                        shape: BoxShape.circle,
-                                                        image: DecorationImage(
-                                                            image: Image.asset("images/wolf_user.png").image,
-                                                            // image: NetworkImage(
-                                                            //     snapshot.data?.docs[index].get("ph")),
-                                                            fit: BoxFit
-                                                                .fill)))),
-                                          ],
-                                        ),
-                                      ),
-                                      Padding(
-                                        padding: const EdgeInsets.only(
-                                            left: 20.0, top: 10.0),
-                                        child: Column(
-                                          mainAxisAlignment:
-                                          MainAxisAlignment.center,
-                                          crossAxisAlignment:
-                                          CrossAxisAlignment.start,
-                                          children: <Widget>[
-                                            Container(
-                                                alignment: Alignment.centerLeft,
-                                                child: Text(users.username,
-                                                  style: TextStyle(
-                                                      color: Colors.deepPurple,
-                                                      fontWeight: FontWeight
-                                                          .w500), maxLines: 6,)
-                                            ),
-                                            Text("Points: " +
-                                                points),
-                                          ],
-                                        ),
-                                      ),
-                                    ],
-                                  ),
-                                ],
+                              // child: Column(
+                              //   children: <Widget>[
+                              //     Row(
+                              //       children: <Widget>[
+                              //         Padding(
+                              //           padding: const EdgeInsets.only(
+                              //               top: 10.0, left: 15.0),
+                              //           child: Row(
+                              //             children: <Widget>[
+                              //               CircleAvatar(
+                              //                   child: Container(
+                              //                       decoration: BoxDecoration(
+                              //                           shape: BoxShape.circle,
+                              //                           image: DecorationImage(
+                              //                               image: Image.asset("images/wolf_user.png").image,
+                              //                               // image: NetworkImage(
+                              //                               //     snapshot.data?.docs[index].get("ph")),
+                              //                               fit: BoxFit
+                              //                                   .fill)))),
+                              //             ],
+                              //           ),
+                              //         ),
+                              //         Padding(
+                              //           padding: const EdgeInsets.only(
+                              //               left: 20.0, top: 10.0),
+                              //           child: Column(
+                              //             mainAxisAlignment:
+                              //             MainAxisAlignment.center,
+                              //             crossAxisAlignment:
+                              //             CrossAxisAlignment.start,
+                              //             children: <Widget>[
+                              //               Container(
+                              //                   alignment: Alignment.centerLeft,
+                              //                   child: Text(users.username,
+                              //                     style: TextStyle(
+                              //                         color: Colors.deepPurple,
+                              //                         fontWeight: FontWeight
+                              //                             .w500), maxLines: 6,)
+                              //               ),
+                              //               Text("Points: " +
+                              //                   points),
+                              //             ],
+                              //           ),
+                              //         ),
+                              //       ],
+                              //     ),
+                              //   ],
                               ),
                             ),
-                          ),
+                          //),
                         );
                       }
                   );
